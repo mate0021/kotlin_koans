9df62e2412401e6ce4606978cf9e45e56f9b2251
mate00 <mami.ext@gmail.com>
2019-10-22 06:32:01 +0200

    Daily Kotlin Koans. Java code converter, default method parameters

--- Introduction/Java to Kotlin conversion/src/Task.kt
+++ Introduction/Java to Kotlin conversion/src/Task.kt
@@ -1,1 +1,18 @@
-fun toJSON(collection: Collection<Int>): String = TODO()
+import kotlin.text.StringBuilder
+
+fun toJSON(collection: Collection<Int>): String {
+    val sb = StringBuilder()
+    sb.append("[")
+    val iterator = collection.iterator()
+    while (iterator.hasNext())
+    {
+        val element = iterator.next()
+        sb.append(element)
+        if (iterator.hasNext())
+        {
+            sb.append(", ")
+        }
+    }
+    sb.append("]")
+    return sb.toString()
+}
