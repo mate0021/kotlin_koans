eb648637fafcc78204022e3469c4d51b4bec7f3c
mate00 <mami.ext@gmail.com>
2019-10-25 06:45:18 +0200

    Daily Kotlin Koans. Object expressions.

--- Introduction/Object expressions/src/Task.kt
+++ Introduction/Object expressions/src/Task.kt
@@ -2,6 +2,14 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList, object: Comparator<Int> {
+        override fun compare(e1: Int, e2: Int) =
+                if (e1 < e2) 1
+        else if (e1 == e2) 0
+        else -1
+    })
     return arrayList
 }
+
+// better:
+// override fun compare(e1: Int, e2: Int) = e2 - e1
