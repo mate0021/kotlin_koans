10d6ccae51cb455a0b3c1e19e2d6a9f36dc397cb
mate00 <mami.ext@gmail.com>
2019-10-26 07:23:11 +0200

    Daily Kotlin Koans. In range.

--- Conventions/In range/src/Task.kt
+++ Conventions/In range/src/Task.kt
@@ -1,5 +1,13 @@
-class DateRange(val start: MyDate, val endInclusive: MyDate)/* TODO */
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    operator fun contains(d: MyDate): Boolean {
+        return start <= d && d <= endInclusive
+    }
+}
 
 fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
     return date in DateRange(first, last)
 }
+
+/*
+return d in start..endInclusive
+ */
