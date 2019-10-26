0edf5a9d8793937535135a785c80350ea81326a4
mate00 <mami.ext@gmail.com>
2019-10-26 06:58:01 +0200

    Daily Kotlin Koans. Collections extension functions. Operator overloading.

--- Conventions/Comparison/src/Task.kt
+++ Conventions/Comparison/src/Task.kt
@@ -1,5 +1,27 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override operator fun compareTo(other: MyDate): Int {
+        if (year < other.year) {
+            return -1
+        } else if (year == other.year && month < other.month) {
+            return -1
+        } else if (year == other.year && month == other.month && dayOfMonth < other.dayOfMonth) {
+            return -1
+        } else if (year == other.year && month == other.month && dayOfMonth == other.dayOfMonth) {
+            return 0
+        } else {
+            return 1
+        }
+
+    }
 }
 
 fun compare(date1: MyDate, date2: MyDate) = date1 < date2
+
+/*
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+
+ */
