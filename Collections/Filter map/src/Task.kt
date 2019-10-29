cc17aef179f0ca819d68dc2b467dc41d966ebdaf
mate00 <mami.ext@gmail.com>
2019-10-29 06:04:18 +0100

    Daily Kotlin Koans. Collections and filters.

--- Collections/Filter map/src/Task.kt
+++ Collections/Filter map/src/Task.kt
@@ -1,5 +1,11 @@
 // Return the set of cities the customers are from
-fun Shop.getCitiesCustomersAreFrom(): Set<City> = TODO()
+fun Shop.getCitiesCustomersAreFrom(): Set<City> = customers.map(Customer::city).toSet()
 
 // Return a list of the customers who live in the given city
-fun Shop.getCustomersFrom(city: City): List<Customer> = TODO()
+fun Shop.getCustomersFrom(city: City): List<Customer> =
+        customers.filter { customer -> customer.city.equals(city) }
+
+/*
+customers.map { it.city }.toSet()
+customers.filter { it.city == city }
+*/
