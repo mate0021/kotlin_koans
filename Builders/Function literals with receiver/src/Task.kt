5beff7ff077562fb5275da1a38e66781f70701ed
mate0021 <mami.ext@gmail.com>
2019-11-12 06:46:36 +0100

    Daily Kotlin Koans. Function literals with receiver.

--- Builders/Function literals with receiver/src/Task.kt
+++ Builders/Function literals with receiver/src/Task.kt
@@ -1,6 +1,10 @@
 fun task(): List<Boolean> {
-    val isEven: Int.() -> Boolean = { TODO() }
-    val isOdd: Int.() -> Boolean = { TODO() }
+    val isEven: Int.() -> Boolean = { this.rem(2) == 0 }
+    val isOdd: Int.() -> Boolean = { this.rem(2) != 0 }
 
     return listOf(42.isOdd(), 239.isOdd(), 294823098.isEven())
 }
+
+/*
+  this % 2 == 0
+*/
