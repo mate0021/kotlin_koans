d8d95c5e966d53f92a921636bfca9bcdcd5f8a2e
mate0021 <mami.ext@gmail.com>
2019-11-15 06:34:10 +0100

    Daily Kotlin Koans. Figuring out generics and partitioning.

--- Generics/Generic functions/src/Task.kt
+++ Generics/Generic functions/src/Task.kt
@@ -2,6 +2,16 @@ import java.util.*
 
 fun partitionTo() = TODO()
 
+/*
+ * 1) partitionTo jest wołane na listOf. To znaczy, że jest to extension function na List.
+ * 2) Jest też wołane na setOf. Więc jest to ogólnie extension na Collection.
+ * 3) Mamy mieć trzy argumenty: dwie kolekcje i predykat.
+ * fun <T, C> Collection<T>.partitionTo(first: T, second: T, predicate: (C) -> Boolean)
+ * 
+ * 4) Zwracamy destructured (words, lines), więc zwracamy Parę
+ * fun <T, C> Collection<T>.partitionTo(first: T, second: T, predicate: (C) -> Boolean): Pair<T, T>
+ */
+
 fun partitionWordsAndLines() {
     val (words, lines) = listOf("a", "a b", "c", "d e").
             partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
